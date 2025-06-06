170476516b2d82d40e411560a2131b65b03e475e
nkietwee <nawaporn.kiet@gmail.com>
2025-06-06 11:30:36 +0700

    addex00

---
+++ ex00/Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World"]
+ft_tuple = ("Hello", "Thailand")
+ft_set = {"Hello", "Bangkok"}
+ft_dict = {"Hello" : "42Bangkok"}
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
